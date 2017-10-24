8ac4193dca2604f3f2e87ba2a7daeb8fde293f66
Joe Dickson <dicksonjoe7@gmail.com>
2017-10-24 13:41:30 -0700

    confirmEnding

--- js-tests.js
+++ js-tests.js
@@ -3,23 +3,12 @@ const show = x => console.log(x);
 /* ======================================================= */
 
 
-function largestOfFour(arr) {
-    // You can do this!
-    var newArr = [];
-    for (i = 0; i <= arr.length - 1; i++) {
-        tempArr = arr[i];
-        tempArr.sort((function(a, b) { return b - a }));
-        newArr.push(parseInt(tempArr[0]));
-    }
-    show(newArr);
-    return newArr;
+function confirmEnding(str, target) {
+    // "Never give up and good luck will find you."
+    // -- Falcor
+
+    return (target === str.substr(str.length - target.length, str.length));
+
 }
 
-largestOfFour([
-    [4, 9, 1, 3],
-    [13, 35, 18, 26],
-    [32, 35, 97, 39],
-    [1000000, 1001, 857, 1]
-])
-
-//should return [9, 35, 97, 1000000]
+confirmEnding("Bastian", "n");
