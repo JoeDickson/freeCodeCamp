15bc8dff4fd091e127ebafe92e0e89a2928baae7
Joe Dickson <dicksonjoe7@gmail.com>
2017-10-24 16:34:03 -0700

    function slasher(arr, howMany) {

--- js-tests.js
+++ js-tests.js
@@ -2,18 +2,10 @@ const show = x => console.log(x);
 
 /* ======================================================= */
 
-
-function chunkArrayInGroups(arr, size) {
-    // Break it up.
-    var retArr = [];
-    var slicedArr = [];
-
-    var res = [];
-    while (arr.length) {
-        retArr.push(arr.splice(0, size));
-    }
-
-    return retArr;
+function slasher(arr, howMany) {
+    // it doesn't always pay to be first
+    newArr = arr.splice(howMany);
+    return newArr;
 }
 
-chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6], 3);
+slasher(["burgers", "fries", "shake"], 1);
