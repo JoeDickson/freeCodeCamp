f3f99a2ddc4abfbb1ce614ecc8461dca3a92ec0d
Joe Dickson <dicksonjoe7@gmail.com>
2017-10-24 16:30:14 -0700

    chunkArrayInGroups

--- js-tests.js
+++ js-tests.js
@@ -6,10 +6,14 @@ const show = x => console.log(x);
 function chunkArrayInGroups(arr, size) {
     // Break it up.
     var retArr = [];
+    var slicedArr = [];
 
-
+    var res = [];
+    while (arr.length) {
+        retArr.push(arr.splice(0, size));
+    }
 
     return retArr;
 }
 
-chunkArrayInGroups(["a", "b", "c", "d"], 2);
+chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6], 3);
