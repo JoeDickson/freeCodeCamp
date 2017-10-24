f2ff4eec453da43d29ab1c455a4512d37bbf58df
Joe Dickson <dicksonjoe7@gmail.com>
2017-10-24 16:28:30 -0700

    chunkArrayInGroups

--- js-tests.js
+++ js-tests.js
@@ -2,17 +2,14 @@ const show = x => console.log(x);
 
 /* ======================================================= */
 
-function truncateString(str, num) {
-    // Clear out that junk in your trunk
 
-    if (str.length > num) {
-        if (num <= 3)
-            return str.slice(0, num) + "...";
-        else
-            return str.slice(0, num - 3) + "...";
-    } else
-        return str;
-    //return 
+function chunkArrayInGroups(arr, size) {
+    // Break it up.
+    var retArr = [];
+
+
+
+    return retArr;
 }
 
-show(truncateString("A-", 1));
+chunkArrayInGroups(["a", "b", "c", "d"], 2);
