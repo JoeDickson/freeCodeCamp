b36aa580fcc4aaa040f3a38e8e2646243429e662
Joe Dickson <dicksonjoe7@gmail.com>
2017-10-24 13:20:02 -0700

    largestOfFour

--- js-tests.js
+++ js-tests.js
@@ -2,14 +2,24 @@ const show = x => console.log(x);
 
 /* ======================================================= */
 
-function titleCase(str) {
-    str = str.toLowerCase();
-    strArr = str.split(" ");
-    for (i = 0; i <= strArr.length - 1; i++) {
-        strArr[i] = strArr[i].charAt(0).toUpperCase() + strArr[i].slice(1);
+
+function largestOfFour(arr) {
+    // You can do this!
+    var newArr = [];
+    for (i = 0; i <= arr.length - 1; i++) {
+        tempArr = arr[i];
+        tempArr.sort((function(a, b) { return b - a }));
+        newArr.push(parseInt(tempArr[0]));
     }
-    str = strArr.join(" ");
-    return str;
+    show(newArr);
+    return newArr;
 }
 
-titleCase("sHoRt AnD sToUt");
+largestOfFour([
+    [4, 9, 1, 3],
+    [13, 35, 18, 26],
+    [32, 35, 97, 39],
+    [1000000, 1001, 857, 1]
+])
+
+//should return [9, 35, 97, 1000000]
