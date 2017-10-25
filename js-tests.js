27bf625ea19d859d25a4b16e7ae271ba0bc82e88
Joe Dickson <dicksonjoe7@gmail.com>
2017-10-25 14:06:58 -0700

    rot13 final

--- js-tests.js
+++ js-tests.js
@@ -1,26 +1,25 @@
 const show = x => console.log(x);
 /* ======================================================= */
 
-function rot13(str) {
-
+function rot13(str) { // LBH QVQ VG!
     var decodedStr = "";
     var strArr = str.split(" ");
     var checkLetters = /^[0-9a-zA-Z]+$/;
-
     for (var i = 0; i <= strArr.length - 1; i++) {
         for (var j = 0; j <= strArr[i].length - 1; j++) {
+
             if (strArr[i][j].match(checkLetters)) {
                 letter = strArr[i][j].charCodeAt(0);
                 if (letter >= 97 && letter <= 22)
+
                     letter = String.fromCharCode((letter - 97 + 13) % 26 + 97);
                 else if (letter >= 65 && letter <= 90)
                     letter = String.fromCharCode((letter - 65 + 13) % 26 + 65);
-
                 decodedStr += letter;
+                return str.replace(/[A-Z]/g, L => String.fromCharCode((L.charCodeAt(0) % 26) + 65));
 
             } else
                 decodedStr += strArr[i][j];
-
         }
         decodedStr += " ";
     }
@@ -28,5 +27,4 @@ function rot13(str) {
 }
 
 // Change the inputs below to test
-rot13("GUR DHVPX OEBJA QBT WHZCRQ BIRE GUR YNML SBK.")
-    //should decode to "THE QUICK BROWN DOG JUMPED OVER THE LAZY FOX.");
+show(rot13("GUR DHVPX OEBJA QBT WHZCRQ BIRE GUR YNML SBK."));
