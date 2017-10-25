192b2c66ae109fa50983b1031a9d7471c8a7350a
Joe Dickson <dicksonjoe7@gmail.com>
2017-10-24 21:06:58 -0700

    filter arguments from array

--- js-tests.js
+++ js-tests.js
@@ -4,10 +4,19 @@ const show = x => console.log(x);
 
 function destroyer(arr) {
     // Remove all the values
-    show(arguments);
-    show(arr);
 
-    return arr;
+    var args = [];
+
+    for (var i = 0; i < arguments.length; i++) {
+        args[i] = arguments[i];
+    }
+    var argArr1 = arguments[0];
+    var argArr2 = args.slice(1);
+    return argArr1.filter(function(x) {
+        return argArr2.indexOf(x) < 0;
+    });
+
 }
 
-destroyer([1, 2, 3, 1, 2, 3], 2, 3);
+show(destroyer([1, 2, 3, 5, 1, 2, 3], 2, 3));
+//should return [1, 1].
