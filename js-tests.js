199e6d84f15cc3dd72fda909cb05c331bc03981c
Joe Dickson <dicksonjoe7@gmail.com>
2017-10-24 19:17:06 -0700

    falsy filter bouncer

--- js-tests.js
+++ js-tests.js
@@ -2,20 +2,10 @@ const show = x => console.log(x);
 
 /* ======================================================= */
 
-function mutation(arr) {
 
-    targetStr = arr[0].toLowerCase();
-    srchStr = arr[1].toLowerCase();
-    boolFound = true;
-    show(targetStr);
-    show(srchStr);
-
-    for (i = 0; i <= srchStr.length - 1; i++) {
-        show(srchStr[i]);
-        if (targetStr.indexOf(srchStr[i]) < 0)
-            boolFound = false;
-    }
-    return boolFound;
+function bouncer(arr) {
+    // Don't show a false ID to this bouncer.
+    return arr.filter(Boolean);
 }
 
-show(mutation(["hello", "hey"]));
+show(bouncer([1, null, NaN, 2, undefined]));
