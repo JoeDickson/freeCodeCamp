eb7ac662b5335980a8470723c65574c2cec6191a
Joe Dickson <dicksonjoe7@gmail.com>
2017-10-27 20:08:19 -0700

    Final Codepen version

--- quotes.js
+++ quotes.js
@@ -3,34 +3,21 @@ const show = x => console.log(x);
 $(document).ready((function() {
         $(".quote").hide();
 
-        //rndNum = Math.floor(Math.random() * quotes.length);
-        //console.log(quotes[rndNum].quoteText);
+        $(".btn-primary").click(function() {
+            $.getJSON("https://codepen.io/dicksonjoe/pen/VrwKRe.html").done(function(data) {
+                rndNum = Math.floor(Math.random() * data.length);
+                var strAuth = data[rndNum].quoteAuthor;
+                var strQuote = data[rndNum].quoteText;
+                var html = "";
+                html += strQuote;
+                if (!strAuth.length)
+                    strAuth = "No Author Credited.";
+                html += "<br />" + strAuth;
 
+                $(".quote").html("<em>" + strQuote + "</em>" + "<br /><center>" + strAuth + "</center> ");
+                $(".quote").show();
+            })
+        })
 
-        $.getJSON("quotes.json").done(function(data) {
-            var items = [];
-            $.each(data, function(key, val) {
-                var strAuth = data[key].quoteAuthor;
-                var strQuote = data[key].quoteText;
-                show(strQuote + ": " + strAuth);
-                //items.push( "<li id='" + key + "'>" + val + "</li>" );
-            });
-        });
-
-
-
-        //{quoteText: "You can't stop the waves, but you can learn to surf.", quoteAuthor: "Jon Kabat-Zinn"}
-
-
-        // $(".btn-primary").click(function() {
-        //     $.getJSON(quotes).done(function(data) {
-        //         for (var key in data) {
-        //             if (data.hasOwnProperty(key)) {
-        //                 var val = data[key];
-        //                 console.log(val);
-        //             }
-        //         }
-        //     });
-        // });
 
     })) // end document ready
